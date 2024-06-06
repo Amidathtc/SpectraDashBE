3198189a5e8614febfd9c3f1dce508f2185e15d4
Amidat <ogunbiyiamidat65@gmail.com>
2024-06-06 02:01:24 +0100

    another commit

--- config/envV.ts
+++ config/envV.ts
@@ -10,3 +10,17 @@ export const EnvironmentVariables = {
   AdminEmail: process.env.AdminEmail as string,
   AdminPassword: process.env.AdminPassword as string,
 };
+
+
+// PORT=1200
+// # MONGO_URL= mongodb+srv://ogunbiyiamidat65:ogunbiyiamidat65@cluster0.tidrfny.mongodb.net/SpectraDash?retryWrites=true&w=majority 
+
+// MONGODB_URI= "mongodb://127.0.0.1:27017/SpectraDashBE"
+// CLOUD_NAME = "dirrncimm"
+// CLOUD_KEY = "198879469219431"
+// CLOUD_SECRET = "U8f7b03CiPBWzJJDjVPU_MF5IJM"
+
+// G_ID = "350112565242-6vg8nj5odo3p0scscedorsbll9t6u613.apps.googleusercontent.com"
+// G_SECRET = "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"
+// G_REFRESH = "1//04CIhzfu8bPUeCgYIARAAGAQSNwF-L9Ir_CY_w6TB_6QNSUxAVlDBGB8CHsLS2b9MixUIwO-secYSvkCCTMgV4d8asYExrzMF5Qo"
+// G_URL = "https://developers.google.com/oauthplayground"
