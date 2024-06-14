177e860b6a8d7279342b12bb10115e8e30d93050
UchennaAustine <uchennaaustine8@gmail.com>
2024-06-14 07:42:20 +0100

    commit

--- Utils/multer.ts
+++ Utils/multer.ts
@@ -1,13 +1,28 @@
-import multer from "multer"
+import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
-  export const upload = multer({ storage: storage }).single("avatar")
+  destination: (req: any, file: any, cb: any) => {
+    cb(null, "uploads");
+  },
+  filename: (req: any, file: any, cb: any) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
+  "avatar"
+);
+
+export default upload;
