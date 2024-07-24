791de8d33c1b097eb9f4bbf2146ee972a4f647e2
UchennaAustine <uchennaaustine8@gmail.com>
2024-07-24 09:01:25 +0100

    extras

--- dist/app.js
+++ dist/app.js
@@ -30,7 +30,7 @@ const MainAppConfig = (app) => {
     app
         .use(express_1.default.json())
         // .use(limiter)
-        .use((0, cors_1.default)({ origin: "*", methods: ["GET, PATCH, POST, DELETE"] }))
+        .use((0, cors_1.default)({ origin: "*", methods: ["GET", "PATCH", "POST", "DELETE"] }))
         .use((0, morgan_1.default)("dev"))
         .use((0, cookie_parser_1.default)())
         .use((req, res, next) => {
